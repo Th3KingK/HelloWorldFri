89ac19c4c910179d5668ba2d6e3a3e35c32e137b
Karam Abuzaid <abuzaid@sheridancollege.ca>
2023-05-26 08:46:35 -0400

    Revert "new class"
    This reverts commit 4b2e2cc6a4c0dd92dfe5e98801c879514518c6ed.

--- src/helloworldfriam/HelloWorldFriAM.java
+++ src/helloworldfriam/HelloWorldFriAM.java
@@ -4,10 +4,6 @@ public class HelloWorldFriAM {
     
     public static void main(String[] args) {
      System.out.println("Hello World");
-     
-    }
-
-    public HelloWorldFriAM() {
     }
     
 }
