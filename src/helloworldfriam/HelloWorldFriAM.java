4b2e2cc6a4c0dd92dfe5e98801c879514518c6ed
Karam Abuzaid <abuzaid@sheridancollege.ca>
2023-05-26 08:45:14 -0400

    new class

--- src/helloworldfriam/HelloWorldFriAM.java
+++ src/helloworldfriam/HelloWorldFriAM.java
@@ -4,6 +4,10 @@ public class HelloWorldFriAM {
     
     public static void main(String[] args) {
      System.out.println("Hello World");
+     
+    }
+
+    public HelloWorldFriAM() {
     }
     
 }
