5d7498b08a35dbb6d393c9a49d8097719c66cc17
Karam Abuzaid <abuzaid@sheridancollege.ca>
2023-05-26 08:48:50 -0400

    Revert "comment https://github.com/Th3KingK/HelloWorldFri.git"
    This reverts commit 91ce8d3b67c586d6c57d8bc7ac20e3704acdecef.

--- src/helloworldfriam/HelloWorldFriAM.java
+++ src/helloworldfriam/HelloWorldFriAM.java
@@ -4,7 +4,6 @@ public class HelloWorldFriAM {
     
     public static void main(String[] args) {
      System.out.println("Hello World");
-     //https://github.com/Th3KingK/HelloWorldFri.git
     }
     
 }
