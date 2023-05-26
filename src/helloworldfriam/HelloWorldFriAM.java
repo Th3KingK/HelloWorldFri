91ce8d3b67c586d6c57d8bc7ac20e3704acdecef
Karam Abuzaid <abuzaid@sheridancollege.ca>
2023-05-26 08:47:32 -0400

    comment https://github.com/Th3KingK/HelloWorldFri.git

--- src/helloworldfriam/HelloWorldFriAM.java
+++ src/helloworldfriam/HelloWorldFriAM.java
@@ -4,6 +4,7 @@ public class HelloWorldFriAM {
     
     public static void main(String[] args) {
      System.out.println("Hello World");
+     //https://github.com/Th3KingK/HelloWorldFri.git
     }
     
 }
